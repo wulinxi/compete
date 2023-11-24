746fd9d1dd8481f3c45a1ff2eedac8fe34523798
linxi <1290978218@qq.com>
2023-11-25 01:24:40 +0800

    chore: 添加注釋

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+// 添加全局樣式
 import '@/styles/index.css';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
@@ -10,8 +11,12 @@ Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
+//  創建一個 vue 項目實例
 new Vue({
+  // 引入路由配置
   router,
   store,
+  // 渲染 App 組件
   render: h => h(App)
+  // 挂載到 #app 節點
 }).$mount('#app')

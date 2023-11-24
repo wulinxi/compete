ab67ad5c0c0a7c4e1456934d03838a6ae74d156b
linxi <1290978218@qq.com>
2023-11-25 01:05:47 +0800

    feat: 添加頁面佈局

---
+++ src/views/Layout.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-container>
+    <el-header>標題</el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu :default-openeds="['1', '3']">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+
+            <el-menu-item>
+              <router-link to="/">Home</router-link>
+            </el-menu-item>
+
+            <el-menu-item>
+              <router-link to="/about">About</router-link>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-main>
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+  
+<script>
+export default {
+  data() {
+    return {
+      //
+    };
+  }
+};
+</script>
+
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
